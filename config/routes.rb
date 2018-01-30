976a29cea8fe8e5c682c1daf09824f10425750ca
brackebusch <kyle.brackebusch@gmail.com>
2018-01-30 14:42:21 -0800

    create user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
-  root to: 'static_pages#root'
-
-  namespace :api, defaults: { format: :json } do
-    resources :users, only: [:index, :show, :create, :destroy]
-    resources :groups, only: [:index, :show, :create, :destory]
-    resources :events, only: [:index, :show, :create, :destory]
-    resource :session, only: [:create, :destroy, :show]
+  Rails.application.routes.draw do
+    namespace :api, defaults: {format: :json} do
+      resource :user, only: [:create, :show]
+      resource :session, only: [:create, :destroy, :show]
+    end
+  
+    root "static_pages#root"
   end
 end
-=======
-end
->>>>>>> 8345681817276c871f0a3317878e916e324b1460
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,6 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def root
+  end
+end
