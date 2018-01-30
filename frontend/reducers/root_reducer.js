fe064cbdccc50a8b24b15ee3c1638b3bc9287e93
brackebusch <kyle.brackebusch@gmail.com>
2018-01-30 15:43:02 -0800

    setup session

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import SessionReducer from './session_reducer';
 
 const rootReducer = combineReducers({
-
+    SessionReducer
 });
 
 export default rootReducer;
